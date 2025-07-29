b1379b814c45ac4b13e4e5736f7cb4df4d526fb9
Mason Kerr <mason.mk@outlook.com>
2025-07-29 23:01:56 +0100

    login somewhat working, logout not returning to home

--- src/QuickEnrol.Client/Program.cs
+++ src/QuickEnrol.Client/Program.cs
@@ -1,11 +1,14 @@
 namespace QuickEnrol.Client
 {
     using Microsoft.AspNetCore.Components.Web;
+    using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using System;
     using System.Net.Http;
     using System.Threading.Tasks;
+    
     public class Program
     {
         public static async Task Main(string[] args)
@@ -15,6 +18,16 @@ namespace QuickEnrol.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.AuthenticationPaths.LogInPath = "authentication/login";
+                options.AuthenticationPaths.LogOutPath = "authentication/logout";
+                options.AuthenticationPaths.LogInCallbackPath = "authentication/login-callback";
+                options.AuthenticationPaths.LogOutCallbackPath = "authentication/logout-callback";
+                options.AuthenticationPaths.RemoteRegisterPath = "/";
+                options.AuthenticationPaths.RemoteProfilePath = "/";
+            });
 
             await builder.Build().RunAsync();
         }
